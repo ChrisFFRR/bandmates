23912c4ca8487fc0f1be18fafa5e6859903b4e9b
chrisFFRR <chris@shotatdawn.net>
2019-03-31 18:14:34 +0200

    initial commit

---
+++ app/src/main/java/no/marchand/bandmates/MainActivity.kt
@@ -0,0 +1,20 @@
+package no.marchand.bandmates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class MainActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+
+}

2df050073577b332dc4f1007509adcfe079daff2
Chris FFRR <chris@shotatdawn.net>
2019-04-01 14:15:56 +0200

    dynamically add login and signup fragment

--- app/src/main/java/no/marchand/bandmates/LoginFragment.kt
+++ app/src/main/java/no/marchand/bandmates/LoginFragment.kt
@@ -1,6 +1,8 @@
 package no.marchand.bandmates
 
 import android.os.Bundle
+
+
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,14 +10,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import kotlin.math.sign
 
 
 class LoginFragment: Fragment() {
-
-    fun newInstance(): LoginFragment {
-        return LoginFragment()
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstance: Bundle?): View? {
         val view =  inflater.inflate(R.layout.fragment_login,container,false)
@@ -27,19 +27,13 @@ class LoginFragment: Fragment() {
         val signupBtn: TextView = view.findViewById(R.id.textView_Signup)
 
         signupBtn.setOnClickListener {
-            var signupFragment: Fragment = SignupFragment()
-            var loginFragment: Fragment? = fragmentManager?.findFragmentById(R.id.login_fragment)
-            var loginTag = loginFragment?.tag.toString()
-
-            Log.d("TAG: ", loginTag)
-
+            val signupFragment: Fragment = SignupFragment()
 
             val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
 
             transaction.replace(R.id.fragment_container, signupFragment)
             transaction.commit()
         }
-
 
 
 
--- app/src/main/java/no/marchand/bandmates/MainActivity.kt
+++ app/src/main/java/no/marchand/bandmates/MainActivity.kt
@@ -2,6 +2,9 @@ package no.marchand.bandmates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -12,7 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val loginFragment: Fragment = LoginFragment()
+        this.setDefaultFragment(loginFragment)
     }
 
 
+   private fun setDefaultFragment(defaultFragment: Fragment) {
+      val manager: FragmentManager = this.supportFragmentManager
+       val transaction: FragmentTransaction = manager.beginTransaction()
+
+       transaction.replace(R.id.fragment_container, defaultFragment)
+       transaction.commit()
+    }
 }

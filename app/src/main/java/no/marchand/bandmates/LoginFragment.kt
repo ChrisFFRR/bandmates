34f64d51581dd401ec8f788f7e3eabd93354ae21
chrisFFRR <chris@shotatdawn.net>
2019-04-01 11:23:58 +0200

    need to replace login with signup

--- app/src/main/java/no/marchand/bandmates/LoginFragment.kt
+++ app/src/main/java/no/marchand/bandmates/LoginFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.FragmentTransaction
 
 
 class LoginFragment: Fragment() {
@@ -22,13 +24,27 @@ class LoginFragment: Fragment() {
         val emailInput: EditText = view.findViewById(R.id.editText_email)
         val passwordInput: EditText = view.findViewById(R.id.editText_pwd)
 
+        val signupBtn: TextView = view.findViewById(R.id.textView_Signup)
+
+        signupBtn.setOnClickListener {
+            var signupFragment: Fragment = SignupFragment()
+            var loginFragment: Fragment? = fragmentManager?.findFragmentById(R.id.login_fragment)
+            var loginTag = loginFragment?.tag.toString()
+
+            Log.d("TAG: ", loginTag)
+
+
+            val transaction: FragmentTransaction = fragmentManager!!.beginTransaction()
+
+            transaction.replace(R.id.fragment_container, signupFragment)
+            transaction.commit()
+        }
 
 
 
 
         Log.d("LOGIN FRAGMENT: ", "email: ${emailInput.text}")
         Log.d("LOGIN FRAGMENT: ", "password: ${passwordInput.text}")
-
 
 
 
